9e595b5ad90b5540548a09ce7aede65e287010d3
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2021-12-28 12:04:43 +0900

    Delete 1000s directory

--- 1000s/1371.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main() {
-    char c;
-    int freq[26] = {0, }, max = 0;
-    while(scanf("%c", &c) != EOF) if(c >= 'a' && c <= 'z') freq[c - 'a']++;
-    for(int i=0; i<26; i++) if(freq[i] > max) max = freq[i];
-    for(int i=0; i<26; i++) if(freq[i] == max) printf("%c", 'a' + i);
-}
