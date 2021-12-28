220e8fe5a7ddffdc667dd75132de67167f2adcf0
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2021-12-28 12:05:32 +0900

    Delete 4000s directory

--- 4000s/4999.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char A[1005], B[1005];
-    scanf("%s %s", A, B);
-    if(strlen(A) >= strlen(B)) printf("go");
-    else printf("no");
-}
