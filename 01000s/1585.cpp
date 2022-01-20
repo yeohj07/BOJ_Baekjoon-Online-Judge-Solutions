f480863b94ba0f9ae31bf0ec3aa773b4157cbf14
yeohj0710 <93759367+yeohj07@users.noreply.github.com>
2022-01-20 10:35:05 +0900

    Add files via upload

--- 01000s/1585.cpp
+++ 01000s/1585.cpp
@@ -1,14 +1,14 @@
-#include <cstdio>
-#include <vector>
-#include <queue>
-#include <algorithm>
+#include cstdio
+#include vector
+#include queue
+#include algorithm
 #define MAX 105
 #define INF 1000000000
 using namespace std;
 
 int N;
 int Capacity[MAX][MAX], Flow[MAX][MAX], SinDist[MAX][MAX];
-vector<int> Line[MAX];
+vectorint Line[MAX];
 
 int MCMF(int Sour, int Sink) {
     int Count = 0, CostSum = 0;
@@ -18,7 +18,7 @@ int MCMF(int Sour, int Sink) {
         fill(Dist, Dist+MAX, INF);
         Dist[Sour] = 0;
 
-        queue<int> Queue;
+        queueint Queue;
         Queue.push(Sour);
         isInQueue[Sour] = true;
 
@@ -26,10 +26,10 @@ int MCMF(int Sour, int Sink) {
             int Cur = Queue.front();
             Queue.pop();
             isInQueue[Cur] = false;
-            for(int i=0; i<Line[Cur].size(); i++) {
+            for(int i=0; iLine[Cur].size(); i++) {
                 int Next = Line[Cur][i];
-                if(Capacity[Cur][Next] - Flow[Cur][Next] > 0
-                   && Dist[Cur] + SinDist[Cur][Next] < Dist[Next]) {
+                if(Capacity[Cur][Next] - Flow[Cur][Next]  0
+                   && Dist[Cur] + SinDist[Cur][Next]  Dist[Next]) {
                     Dist[Next] = Dist[Cur] + SinDist[Cur][Next];
                     Parent[Next] = Cur;
                     if(!isInQueue[Next]) {
@@ -45,53 +45,53 @@ int MCMF(int Sour, int Sink) {
         for(int i=Sink; i!=Sour; i=Parent[i])
             Amount = min(Amount, Capacity[Parent[i]][i] - Flow[Parent[i]][i]);
         for(int i=Sink; i!=Sour; i=Parent[i]) {
-            CostSum += Amount*SinDist[Parent[i]][i];
+            CostSum += AmountSinDist[Parent[i]][i];
             Flow[Parent[i]][i] += Amount;
             Flow[i][Parent[i]] -= Amount;
         }
         Count++;
     }
-    if(Count < N) return -1;
+    if(Count  N) return -1;
     else return CostSum;
 }
 
 int main() {
     int T, F;
-    scanf("%d", &N);
-    int Sour = 2*N+1, Sink = 2*N+2;
-    for(int i=1; i<=N; i++) {
+    scanf(%d, &N);
+    int Sour = 2N+1, Sink = 2N+2;
+    for(int i=1; i=N; i++) {
         Line[Sour].push_back(i), Line[i].push_back(Sour);
         Capacity[Sour][i] = 1;
     }
-    for(int i=N+1; i<=2*N; i++) {
+    for(int i=N+1; i=2N; i++) {
         Line[i].push_back(Sink), Line[Sink].push_back(i);
         Capacity[i][Sink] = 1;
     }
-    vector<int> In(N+1), Out(2*N+1);
-    for(int i=1; i<=N; i++) scanf("%d", &In[i]);
-    for(int i=N+1; i<=2*N; i++) scanf("%d", &Out[i]);
-    scanf("%d %d", &T, &F);
-    for(int i=1; i<=N; i++) {
-        for(int j=N+1; j<=2*N; j++) {
-            if(Out[j] > In[i]) {
+    vectorint In(N+1), Out(2N+1);
+    for(int i=1; i=N; i++) scanf(%d, &In[i]);
+    for(int i=N+1; i=2N; i++) scanf(%d, &Out[i]);
+    scanf(%d %d, &T, &F);
+    for(int i=1; i=N; i++) {
+        for(int j=N+1; j=2N; j++) {
+            if(Out[j]  In[i]) {
                 Line[i].push_back(j), Line[j].push_back(i);
                 Capacity[i][j] = 1;
             }
-            if(Out[j]-In[i] < T) {
-                SinDist[i][j] = min((T-(Out[j]-In[i]))*(T-(Out[j]-In[i])), F);
+            if(Out[j]-In[i]  T) {
+                SinDist[i][j] = min((T-(Out[j]-In[i]))(T-(Out[j]-In[i])), F);
                 SinDist[j][i] = -SinDist[i][j];
             }
         }
     }
     int Ans = MCMF(Sour, Sink);
-    printf("%d ", Ans);
-    if(Ans < 0) return 0;
+    printf(%d , Ans);
+    if(Ans  0) return 0;
 
     fill(&Flow[0][0], &Flow[MAX-1][MAX], 0);
-    for(int i=1; i<=N; i++)
-        for(int j=N+1; j<=2*N; j++) {
+    for(int i=1; i=N; i++)
+        for(int j=N+1; j=2N; j++) {
             SinDist[i][j] = -SinDist[i][j];
             SinDist[j][i] = -SinDist[j][i];
         }
-    printf("%d", -MCMF(Sour, Sink));
+    printf(%d, -MCMF(Sour, Sink));
 }
